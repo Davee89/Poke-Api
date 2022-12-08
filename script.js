0cd9b8deff81aad9fe0fec0358ae31823cd8f517
Dawid Matras <davematras@gmail.com>
2022-12-08 23:18:36 +0100

    card animation added

--- script.js
+++ script.js
@@ -5,15 +5,25 @@ const fetchNewPokemon = (id) => {
     .then((response) => response.json())
     .then((data) => {
       const newPokemonCard = document.createElement("div");
-      newPokemonCard.classList.add("pokemon__card");
+      newPokemonCard.classList.add("pokemon__card", "animated");
       newPokemonCard.setAttribute("data-tilt", "");
-      newPokemonCard.innerHTML = `<h2>${data.name.toUpperCase()}</h2>
-      <img src="${data.sprites.front_default}" />
-      <p></p>
-      <p></p>`;
+      newPokemonCard.innerHTML = `<div class= 'pokemon__image'>
+      <img src="${data.sprites.other.dream_world.front_default}" /></div>
+      <h2 class = 'pokemon__name'>${data.name.toUpperCase()}</h2>
+      <p class = 'pokemon__type'>TYPE: ${data.types[0].type.name.toUpperCase()}</p>
+      <div class = "pokemon__stats">
+      <div class = 'hp'>HP: ${data.stats[0].base_stat}</div>
+      <div class = 'attack'>Attack: ${data.stats[1].base_stat}</div>
+      <div class = 'def'>Defense: ${data.stats[2].base_stat}</div>
+      </div>
+      </div>`;
       console.log(data);
       main.appendChild(newPokemonCard);
-      VanillaTilt.init(document.querySelectorAll(".pokemon__card"));
+      VanillaTilt.init(document.querySelectorAll(".pokemon__card")),
+        {
+          glare: true,
+          scale: 1.2,
+        };
     })
     .catch((error) => {
       console.error(error);
