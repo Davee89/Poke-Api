41f1dc852d8c832b094b4cc1defd693c872aca3a
Dawid Matras <davematras@gmail.com>
2022-12-08 20:15:01 +0100

    added js library

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ const fetchNewPokemon = (id) => {
     .then((data) => {
       const newPokemonCard = document.createElement("div");
       newPokemonCard.classList.add("pokemon__card");
+      newPokemonCard.setAttribute("data-tilt", "");
       newPokemonCard.innerHTML = `<h2>${data.name.toUpperCase()}</h2>
-      <img src="${data.sprites.front_shiny}" />
+      <img src="${data.sprites.front_default}" />
       <p></p>
       <p></p>`;
       console.log(data);
       main.appendChild(newPokemonCard);
+      VanillaTilt.init(document.querySelectorAll(".pokemon__card"));
     })
     .catch((error) => {
       console.error(error);
